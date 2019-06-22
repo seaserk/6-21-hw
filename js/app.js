b0c0138a39629b8b479fe4bc1ee54bb1ea9fc21f
Kyle Seaser <kyleseaser@Kyles-MacBook-Pro.local>
2019-06-22 15:00:41 -0600

    initial commit A + B

---
+++ js/app.js
@@ -0,0 +1,20 @@
+console.log ('working');
+
+// //create variable called firstVariable
+let firstVariable = "hello world";
+// Change the value of this variable to some number.
+firstVariable = 12;
+// Store the value of firstVariable in a new variable called secondVariable
+let secondVariable = firstVariable;
+// Change the value of secondVariable to any string.
+secondVariable = "potato";
+// What is the value of firstVariable?
+//12
+
+
+//Create a variable called yourName and set it equal to your name as a string. 
+let yourName = "Kyle"
+//Then, write an expression that takes the string 
+//"Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+let greeting = `Hello, my name is ${yourName}`;
+	console.log(greeting);
