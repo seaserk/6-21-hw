161b040fc933eca03e7413fa7098dfa32481de4c
Kyle Seaser <kyleseaser@Kyles-MacBook-Pro.local>
2019-06-23 13:33:34 -0600

    printCool

--- js/app.js
+++ js/app.js
@@ -189,26 +189,77 @@ console.log(myArray);
 // 		}
 // 	}
 
-//MONKEY IN TH MIDDLE
-
-if (i<5){
-	console.log("little number"){
-		else {
-			if (i>10) {
-				console.log("big number"){
-					else {
-						console.log("monkey")
-					}
-				}
-			}
-		}
-	}
-}
+//MONKEY IN TH MIDDLE -- HELP WITH SYNTAX
 
 
+// if (i<5){
+// 	console.log("little number"){
+// 		else {
+// 			if (i>10) {
+// 				console.log("big number"){
+// 					else {
+// 						console.log("monkey")
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// }
+
+
+//WHAT"S IN YOUR CLOSET?
+
+// const kristynsCloset = [
+//   "left shoe",
+//   "cowboy boots",
+//   "right sock",
+//   "GA hoodie",
+//   "green pants",
+//   "yellow knit hat",
+//   "marshmallow peeps"
+// ];
+
+// // Thom's closet is more complicated. Check out this nested data structure!!
+// const thomsCloset = [
+//   [
+//     // These are Thom's shirts
+//     "grey button-up",
+//     "dark grey button-up",
+//     "light blue button-up",
+//     "blue button-up",
+//   ],[
+//     // These are Thom's pants
+//     "grey jeans",
+//     "jeans",
+//     "PJs"
+//   ],[
+//     // Thom's accessories
+//     "wool mittens",
+//     "wool scarf",
+//     "raybans"
+//   ]
+// ];
+
+// What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+
+// Kristyn just bought some sweet shades! Add "raybans" to her closet after "yellow knit hat".
+
+// Kristyn spilled coffee on her hat... modify this item to read "stained knit hat" instead of yellow.
+
+// Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirts array.
+
+// In the same way, access one item from Thom's pants array.
+
+// Access one item from Thom's accessories array.
+
+// Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+
+// Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
 
 
 
+//console.log(printGreeting("Slimer"));
 
-
+function printCool(${name} `is cool`))
+console.log(printCool("Captain Ron"));
 
